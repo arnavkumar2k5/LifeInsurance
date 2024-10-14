fdc6784bdef6008f693c4ef0d9857f932f28c6ce
arnavkumar2k5 <144643023+arnavkumar2k5@users.noreply.github.com>
2024-10-14 22:28:37 +0530

    first commit

--- src/Components/Component5.jsx
+++ src/Components/Component5.jsx
@@ -4,7 +4,7 @@ function Component5() {
   return (
     <div className="bg-white pt-10 md:pt-20 p-5 md:p-40">
       <div className="m-auto flex flex-col justify-center">
-        <div className="flex flex-col md:flex-row justify-center items-center gap-10">
+        <div className="flex flex-col-reverse md:flex-row justify-center items-center gap-10">
           <div className="w-full md:w-1/2 flex flex-col gap-5 md:gap-10 text-sm md:text-lg">
             <div className="text-xl md:text-2xl font-bold">
               How much insurance coverage do you need?
@@ -23,7 +23,7 @@ function Component5() {
                 <li>Other insurance you have</li>
               </ul>
             </div>
-            <div className="pt-2 pb-2 rounded-lg text-sm text-center text-white bg-cyan-400 w-[50%] md:w-[40%]">
+            <div className="pt-2 pb-2 m-auto rounded-lg text-sm text-center text-white bg-cyan-400 w-[50%] md:w-[40%]">
               Get a Quote
             </div>
           </div>
